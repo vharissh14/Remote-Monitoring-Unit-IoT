67be316d3a11c3223369e355a8609c5a40f43fe7
Riverway <riverway@elpisitsolutions.com>
2019-11-01 17:41:36 +0530

    modal for edit added

--- public/js/demo/datatables-demo.js
+++ public/js/demo/datatables-demo.js
@@ -1,4 +1,12 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
   $('#dataTable').DataTable();
+
+  $('#myModal').on('show.bs.modal', function(e) {
+
+    //get data-id attribute of the clicked element
+    var bookId = $(e.relatedTarget).data('book-id');
+    //populate the textbox
+    $('#mypid').text('1233');
+});
 });
